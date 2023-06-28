9c555221ca38218e61c8e92843597ac137c59927
chandan-tarami <taramimagardipesh8848@gmail.com>
2023-06-28 17:42:13 +0545

    day2.2

---
+++ video.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video</title>
+    <link rel="stylesheet" href="./style/video.css">
+</head>
+<body>
+    <?php include './components/register.php';?>
+    <?php include './components/login.php';?>
+    <?php include './components/navbar.php';?>
+    <?php include './components/option.php';?>
+
+     <section class="video_section">
+        <div class="video_left">
+            <video src="" ></video>
+        </div>
+
+        <div class="video_right">
+
+            <div class="nav1">
+                <h1>Top Anime</h1>
+            </div>
+
+            <div class="top_list">
+
+            <div>
+                <div class="top1">
+                    <div class="num1">
+                        1
+                    </div>
+                    <img src="./images/onepiece.jpg" >
+                    <div class="pag1">
+                        <h2>One Piece</h2>
+                    </div>
+                </div>
+
+
+                <div class="top2">
+                    <div class="num2">2</div>
+                    <img src="./images/jujutsu.jpg" >
+                    <div class="pag2">
+                        <h2>Jujutsu Kaisen</h2>
+                    </div>
+                </div>
+
+
+                <div class="top3">
+                    <div class="num3">3</div>
+                    <img src="./images/mashle.jpg" >
+                    <div class="pag3">
+                        <h2>Mashle</h2>
+                    </div>
+                </div>
+
+                <div class="top4">
+                    <div class="num4">4</div>
+                    <img src="./images/demonslayer.jpg" >
+                    <div class="pag4">
+                        <h2>Demon Slayer</h2>
+                    </div>
+                </div>
+
+                <div class="top5">
+                    <div class="num5">5</div>
+                    <img src="./images/hells.jpg" >
+                    <div class="pag5">
+                        <h2>Hell's Paradise</h2>
+                    </div>
+                </div>
+
+            </div>
+           
+        </div>
+        </div>
+
+    </section>
+
+    <section >
+
+    </section>
+    
+</body>
+</html>
